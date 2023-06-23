09f8ac3f986343c444345fc91d1f0f08377e2357
Sean Wong <sean.uuong@gmail.com>
2023-06-22 20:37:01 -0700

    Flesh out userProfile and userHistory

--- src/lib/UserHistoryElement.jsx
+++ src/lib/UserHistoryElement.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 const UserHistoryElement = (props) => {
-  return ();
+  return (
+    <div className='history-element'>
+      <div className='location-name'></div>
+      <div className='review-content'>
+        <div className='review-stars'></div>
+        <div className="review-date"></div>
+        <div className="review-drink"></div>
+      </div>
+    </div>
+  );
 };
 
 export default UserHistoryElement;
--- src/lib/UserProfile.jsx
+++ src/lib/UserProfile.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
+import UserHistoryList from './UserHistoryList.jsx';
+
 const UserProfile = (props) => {
   const [profile, setProfile] = useState([]);
 
-  var button = <div></div>
+  // var button = <div></div>
 
   return (
     // only thing that is different between friend and user
@@ -13,10 +15,15 @@ const UserProfile = (props) => {
 
     <div className="profile">
       <div className='profile-info'>
-
+        <div className='profile-picture'>
+          <img alt=''></img>
+        </div>
+        <div className='profile-username'></div>
+        <div className='profile-biography'></div>
+        <div className='profile-button'></div>
       </div>
       <div className='profile-history'>
-
+        <UserHistoryList />
       </div>
     </div>
   )
