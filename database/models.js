1218b823777b634d8ebe109b4974937d595439ad
Sean Wong <sean.uuong@gmail.com>
2023-06-24 11:26:04 -0700

    Change env stuff

--- database/models.js
+++ database/models.js
@@ -10,7 +10,7 @@ import 'dotenv/config';
 
 (async () => {
   try {
-    await mongoose.connect(process.env.DATABASE);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('Database successfully connected');
   } catch (err) {
     console.log('error: ' + err)
