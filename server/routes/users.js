9fc925c9b2be79bd8c5ea2bc650acec4c8133a83
Sean Wong <sean.uuong@gmail.com>
2023-06-26 14:59:08 -0700

    Returns user info from user name

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,8 +4,17 @@ const router = express.Router();
 import { getUser } from '../../database/controllers/user.js';
 
 router.get('/:name', async (req, res) => {
-  var userInfo = await getUser(req.params);
-  res.send(userInfo);
+  return await getUser(req.params)
+    .then((result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.send('User not found');
+      }
+    })
+    .catch((err) => {
+      res.sendStatus(404);
+    });
 })
 
 export default router;
