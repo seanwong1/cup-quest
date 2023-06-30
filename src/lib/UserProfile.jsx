909f4a6f3cff0ee1403f909dd4af2384146d2309
Sean Wong <sean.uuong@gmail.com>
2023-06-29 21:37:14 -0700

    Begin implementing chat server

--- src/lib/UserProfile.jsx
+++ src/lib/UserProfile.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 
 import UserHistoryList from './UserHistoryList.jsx';
 import FriendToggle from './FriendToggle.jsx';
+import ChatRoom from './ChatRoom.jsx';
 
 import requestHandler from './requestHandler.js';
 
@@ -41,7 +42,10 @@ const UserProfile = (props) => {
           <button className="friends-button" >Friends</button>
         </Link>
         {isUser ? <div className='edit-button' onClick={() => {console.log('hi')}}>Edit User</div> :
-          <FriendToggle id={profile._id} />
+          <div>
+            <FriendToggle id={profile._id} />
+            <ChatRoom id={profile._id} />
+          </div>
         }
       </div>
       <div className='profile-history'>
---
+++ src/pages/ChatRoom.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from 'react';
+
+const ChatRoom = (props) => {
+
+  return (
+    <div>hello</div>
+  )
+}
+
+export default ChatRoom;
