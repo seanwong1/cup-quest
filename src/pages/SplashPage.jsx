6d8ba5c92b773f01789f249990fca50cc8eb49cb
George Halterman <113994972+Agoews@users.noreply.github.com>
2023-07-01 13:16:14 -0700

    saving before pull from main

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import GoogleSignIn from './firebase/GoogleSignIn';
+import GoogleSignIn from './firebase/googleSignIn';
 
 import lightDarkToggle from '../lib/lightDarkToggle.js';
 
