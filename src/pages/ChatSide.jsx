77602ee7cb12673f2fcfb2b9f362eaae2cb90269
Sean Wong <sean.uuong@gmail.com>
2023-07-04 11:23:30 -0700

    Clicking on user chats that user

--- src/pages/ChatSide.jsx
+++ src/pages/ChatSide.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import FriendsList from './FriendsList.jsx';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams, useLocation } from 'react-router-dom';
+import FriendElement from './FriendElement.jsx';
 
-const ChatSide = () => {
+import requestHandler from '../lib/requestHandler.js';
+
+const ChatSide = ({ currentUser, setCurrentChat }) => {
+  const [friends, setFriends] = useState([]);
+  // TESTING
+  const [users, setUsers] = useState([]);
+  const { name } = useParams();
+
+  useEffect(() => {
+    requestHandler(`/user/${currentUser.name}/friends`, null, 'get', (response) => {
+      setFriends(response.data);
+    });
+  }, [currentUser.name]);
+
   return (
     <div className='chat-side'>
-      <FriendsList />
+      <h1>Friends</h1>
+        {friends.map((friend) => {
+          return (
+            <div onClick={() => {
+              setCurrentChat(friend.name);
+            }}>{friend.name}</div>
+          )
+        })}
     </div>
   )
 }
