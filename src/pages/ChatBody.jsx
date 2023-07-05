124d9e396469fb5930ac2002b40ef32f1ed3cde0
Sean Wong <sean.uuong@gmail.com>
2023-07-04 17:25:07 -0700

    Add ternary operator to prevent rendering undefined

--- src/pages/ChatBody.jsx
+++ src/pages/ChatBody.jsx
@@ -3,20 +3,14 @@
 import React, { useState } from 'react';
 import ChatMessage from './ChatMessage.jsx';
 
-const ChatBody = ({ socket }) => {
-  const [messages, setMessages] = useState([]);
-
-  socket.on('receive_message', (data) => {
-    setMessages([...messages, data]);
-  });
-
+const ChatBody = ({ socket, messages }) => {
   return (
     <div className='chat-body'>
-      {messages.map((message) => {
+      {messages ? messages.map((message) => {
         return (
           <ChatMessage message={message} />
         )
-      })}
+      }) : null}
     </div>
   )
 }
