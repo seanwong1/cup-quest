f146b3f0db00a0db698df19772825514472d8160
Sean Wong <sean.uuong@gmail.com>
2023-07-04 17:28:14 -0700

    Select chat by clicking on friend

--- src/pages/ChatSide.jsx
+++ src/pages/ChatSide.jsx
@@ -4,20 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams, useLocation } from 'react-router-dom';
 import FriendElement from './FriendElement.jsx';
 
-import requestHandler from '../lib/requestHandler.js';
-
-const ChatSide = ({ currentUser, setCurrentChat }) => {
-  const [friends, setFriends] = useState([]);
-  // TESTING
-  const [users, setUsers] = useState([]);
-  const { name } = useParams();
-
-  useEffect(() => {
-    requestHandler(`/user/${currentUser.name}/friends`, null, 'get', (response) => {
-      setFriends(response.data);
-    });
-  }, [currentUser.name]);
-
+const ChatSide = ({ friends, setCurrentChat }) => {
   return (
     <div className='chat-side'>
       <h1>Friends</h1>
