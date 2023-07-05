8c89c66c52481316ab78ae04bdaa20f5023fb350
Sean Wong <sean.uuong@gmail.com>
2023-07-04 18:06:04 -0700

    Convert date to human readable

--- src/pages/ChatMessage.jsx
+++ src/pages/ChatMessage.jsx
@@ -2,12 +2,14 @@
 import React, { useState } from 'react';
 
 const ChatMessage = ({ message }) => {
+  const dateObject = new Date(message.timeStamp);
+  const humanDateFormat = dateObject.toLocaleString();
 
   return (
     <div className='chat-message'>
       <div className='chat-message-username'>{message.username}</div>
       <div className='chat-message-message'>{message.text}</div>
-      <div className='chat-message-timestamp'>{message.timeStamp}</div>
+      <div className='chat-message-timestamp'>{humanDateFormat}</div>
     </div>
   )
 }
