12b9ffe2268324d9abee40427fe29b2da5a37732
Sean Wong <sean.uuong@gmail.com>
2023-07-04 17:25:57 -0700

    Add a message componenet

--- src/pages/ChatMessage.jsx
+++ src/pages/ChatMessage.jsx
@@ -2,10 +2,10 @@
 import React, { useState } from 'react';
 
 const ChatMessage = ({ message }) => {
-  console.log(message);
+
   return (
     <div className='chat-message'>
-      <div className='chat-message-username'></div>
+      <div className='chat-message-username'>{message.username}</div>
       <div className='chat-message-message'>{message.text}</div>
       <div className='chat-message-timestamp'>{message.timeStamp}</div>
     </div>
